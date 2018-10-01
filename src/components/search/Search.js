f574c6a8b337c0426ab6c41b85ac38b6ffd44769
SeanKim <sean.kim84@gmail.com>
2018-10-01 14:33:03 +0700

    Adding the Condition to the onChange

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -16,13 +16,18 @@ class Search extends Component {
     }
 
     onTextChange = (e) => {
-        this.setState({[e.target.name]: e.target.value}, () => {
-            Axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+        const val = e.target.value;
+        this.setState({[e.target.name]: val}, () => {
+            if(val=== '') {
+                this.setState({images:[]})
+            } else {
+                Axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
                 this.state.searchText
             }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
             )
             .then(res => this.setState({ images: res.data.hits }))
             .catch(err => console.log(err));
+            }
         });
     } 
 
